ed66b8cfccea2ef7c828d64a0119257d846ee402
Raphaella Morais <raphaellakm@gmail.com>
2022-05-24 09:45:21 -0300

    curso-completo

---
+++ src/app/models/transferencia.model.ts
@@ -0,0 +1,13 @@
+//declare module namespace {
+export interface Transferencia {
+      id?: string;
+      valor: number;
+      destino: number | string;
+      data?: Date;
+  }
+
+  //export interface RootObject {
+    //  transferencias: Transferencia[];
+  //}
+
+//}
